cc38ee1c06ee558bb73cc145b44e3307d2200d6b
Akram <muhammadakram00006@gmail.com>
2024-02-15 11:33:14 +0500

    Time-stamps-added

--- models/issueModel.js
+++ models/issueModel.js
@@ -1,61 +1,63 @@
 import mongoose, { ObjectId } from "mongoose";
 
-const issueSchema=new mongoose.Schema({
-    issueName:{
-        type:String,
-        //required:true
+const issueSchema = new mongoose.Schema(
+  {
+    issueName: {
+      type: String,
+      //required:true
     },
-    type:{
-           type:String,
+    type: {
+      type: String,
     },
-    issueDescription:{
-        type:String,
-        //required:true
-        default:"high"
+    issueDescription: {
+      type: String,
+      //required:true
+      default: "high",
     },
-    priority:{
-        type:String,
-        //required:true
+    priority: {
+      type: String,
+      //required:true
     },
-    startDate:{
-        type:Date,
-        //required:true
+    startDate: {
+      type: Date,
+      //required:true
     },
     Assignedto: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "users",
-        default: null // Default value is null
-      },
-      
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      default: null, // Default value is null
+    },
 
-    completionDate:{
-        type:Date,
-        //required:true
+    completionDate: {
+      type: Date,
+      //required:true
     },
-    comment:{
-        type:String,
-        //required:true
+    comment: {
+      type: String,
+      //required:true
     },
-    status:{
-        type:String,
-        default:"pending"
+    status: {
+      type: String,
+      default: "pending",
     },
     // userId:{
     //     type: mongoose.Schema.Types.ObjectId,
     //     ref: "users"
     // },
     projectId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "projects"
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "projects",
     },
-    userId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "users" 
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
     },
     date: {
-        type: Date,
-        default: Date.now, // Set the default value to the current date and time
-    }
-})
-const issueModel=mongoose.model("issue",issueSchema)
+      type: Date,
+      default: Date.now, // Set the default value to the current date and time
+    },
+  },
+  { timestamps: true }
+);
+const issueModel = mongoose.model("issue", issueSchema);
 export default issueModel;
